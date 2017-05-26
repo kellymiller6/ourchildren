16fdf3587252898ecab7369884648df61cd52b83
Kelly Miller <kellyannemiller@gmail.com>
2017-05-26 16:44:34 -0600

    add component files

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+// import quotes from '../../quotes/quotes.json'
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <h1>Our Children</h1>
-        <p>quotes</p>
+        <p></p>
       </div>
     );
   }
