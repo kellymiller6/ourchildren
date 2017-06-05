9bfdd6d54e721b31642c8aa2a4b78be08378cd0b
Kelly Miller <kellyannemiller@gmail.com>
2017-06-05 11:52:08 -0600

    remove console log

--- src/components/AuthPages/Home.js
+++ src/components/AuthPages/Home.js
@@ -3,7 +3,6 @@ import { quotes } from '../../quotes/quotes.js'
 
 const Home = () => {
   const display = quotes[Math.floor(Math.random() * quotes.length)];
-  console.log(display);
   return (
    <div className='quotes Home'>
     <h3>{display.Quote}</h3>
